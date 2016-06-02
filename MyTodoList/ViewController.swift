00fc590bee67fd035f17a5d0317c11afc5abca13
Shiozawa Tomo <salt@Shiozawa-no-MacBook-Air.local>
2016-06-02 17:38:36 +0900

    とりあずタスク追加の大枠完成

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate{
 
     //TODOを格納する配列
     var todoList = [MyTodo]()
@@ -16,10 +16,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //入力用のTextField
     var titleTextField: UITextField?
     var deadlineTextField: UITextField?
+    var weightTextField: UITextField?
     
-    //UIDatePickerの用意
+    //UIDatePickerの用意(deadline入力用)
     let datePicker = UIDatePicker()
+    
+    //UIPickerviewの用意(weight入力用)
+    let weightPicker = UIPickerView()
+    var weightOption = ["ASAP","Normal","In the future"]
+    
+    //Pickerのツールバー
     var pickerToolBar = UIToolbar()
+    
+    //日付データのフォーマット用
+    let dateFormatter = NSDateFormatter()
+    
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -34,19 +45,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         //datePickerの設定
-        self.datePicker.datePickerMode = UIDatePickerMode.Date
-        self.datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        datePicker.datePickerMode = UIDatePickerMode.Date
+        datePicker.locale = NSLocale(localeIdentifier: NSLocale.currentLocale().localeIdentifier)
+        datePicker.minimumDate = NSDate()
+        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
         
-        // datePickerに表示するツールバーのいろいろ
+        //weightPickerの設定
+        weightPicker.delegate = self
+        
+        // 各種Pickerに表示するツールバーの設定
         pickerToolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
         pickerToolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
         pickerToolBar.barStyle = .BlackTranslucent
         pickerToolBar.tintColor = UIColor.whiteColor()
         pickerToolBar.backgroundColor = UIColor.blackColor()
         
-        //datePickerのツールバーに完了ボタンを設定
+        //各種Pickerのツールバーに完了ボタンを設定
         let okBarBtn = UIBarButtonItem(title: "OK", style: .Done, target: self, action: #selector(ViewController.okBarBtnPush(_:)))
-        pickerToolBar.items = [okBarBtn]
+        let spaceBarBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace,target: self,action: Selector())
+        pickerToolBar.items = [spaceBarBtn,okBarBtn]
+        
+        
+        //dateFotmatterの設定
+        dateFormatter.locale = NSLocale(localeIdentifier: NSLocaleLanguageCode)
+        dateFormatter.dateFormat = "yyyy/MM/dd"
 
     }
 
@@ -62,15 +84,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let alertController = UIAlertController(title: "TODO追加", message: "TODOを入力してください", preferredStyle: UIAlertControllerStyle.Alert)
         
         //テキストエリアを追加
+        //title用
         alertController.addTextFieldWithConfigurationHandler( { (title: UITextField!) -> Void in
             self.titleTextField = title
             title.placeholder = "Title"
         })
+        //deadline用
         alertController.addTextFieldWithConfigurationHandler( { (deadline: UITextField!) -> Void in
             self.deadlineTextField = deadline
             deadline.placeholder = "Deadline"
             deadline.inputView = self.datePicker
             deadline.inputAccessoryView = self.pickerToolBar
+        })
+        //weight用
+        alertController.addTextFieldWithConfigurationHandler( { (weight: UITextField!) -> Void in
+            self.weightTextField = weight
+            weight.placeholder = "weight"
+            weight.inputView = self.weightPicker
+            weight.inputAccessoryView = self.pickerToolBar
         })
         
         //OKボタンが押された時の処理
@@ -87,6 +118,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 myTodo.title = alertController.textFields![0].text!
                 //textFieldsの2個目がDeadline
                 myTodo.deadline = alertController.textFields![1].text!
+                //作成日時を取得してcreatedAtに入れる
+                myTodo.createdAt = self.dateFormatter.stringFromDate(NSDate())
+                //重要度を入れる
+                let weightText = alertController.textFields![2].text!
+                if weightText == "ASAP" {
+                    myTodo.weight = 3
+                }
+                else if weightText == "Normal" {
+                    myTodo.weight = 2
+                }
+                else if weightText == "In the future" {
+                    myTodo.weight = 1
+                }
                 //todoListに追加
                 self.todoList.insert(myTodo, atIndex: 0)
             }
@@ -119,10 +163,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     /*
-    datePickerのOKボタンを押した時の処理
+    PickerのOKボタンを押したらPickerを消す
     */
     func okBarBtnPush(sender: AnyObject?){
         deadlineTextField!.resignFirstResponder()
+        weightTextField!.resignFirstResponder()
     }
     
     
@@ -130,12 +175,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     datePickerが変更されたきの処理
     */
     func datePickerValueChanged(sender:UIDatePicker) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
-        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        //deadline用のTextFieldに日付を入れる
         deadlineTextField!.text = dateFormatter.stringFromDate(sender.date)
-        
     }
+    
+    
+    /*
+    weightPickeのために必要な関数4つ
+    */
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return weightOption[row]
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return weightOption.count
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        weightTextField!.text = weightOption[row]
+    }
+    
 
     /*
     テーブルの行数を返す
@@ -153,7 +213,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //storyboardで指定したtodoCell識別子を利用して再利用可能なセルを取得する(メモリを有効活用するための処理らしい、勉強不足)
         let cell = tableView.dequeueReusableCellWithIdentifier("todoCell", forIndexPath: indexPath)
         
-        //行番号に合ったTODOのタイトルを取得
+        //行番号に合ったTODOを取得
         let todo = todoList[indexPath.row]
         
         //セルのラベルにTODOのタイトルをセット
@@ -186,9 +246,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         else {
             //未完の場合は完了済みに変更
             todo.status = true
+            //完了した日付をdoneAtに入れる
+            todo.doneAt = self.dateFormatter.stringFromDate(NSDate())
         }
         
-        //セルの状態を変更
+        //セルの状態を更新
         tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
         
         //データ保存
@@ -247,14 +309,14 @@ class MyTodo: NSObject, NSCoding {
     //Todoの締め切り
     var deadline :String?
     
+    //重要度
+    var weight :Int32?
+    
     //作成した日時    
     var createdAt :String?
     
     //完了した日時
     var doneAt :String?
-    
-    //重要度
-    var weight :String?
     
     //コンストラクタ
     override init() {
@@ -264,13 +326,21 @@ class MyTodo: NSObject, NSCoding {
     //デシリアライズ処理(デコード)
     required init?(coder aDecoder: NSCoder) {
         title = aDecoder.decodeObjectForKey("todoTitle") as? String
-        status = aDecoder.decodeBoolForKey("todoDone")
+        status = aDecoder.decodeBoolForKey("todoStatus")
+        deadline = aDecoder.decodeObjectForKey("todoDeadline") as? String
+        weight = aDecoder.decodeIntForKey("todoWeight") as? Int32
+        createdAt = aDecoder.decodeObjectForKey("todoCreatedAt") as? String
+        doneAt = aDecoder.decodeObjectForKey("todoDoneAt") as? String
     }
     
     //シリアライズ処理(エンコード)
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(title, forKey: "todoTitle")
-        aCoder.encodeBool(status, forKey: "todoDone")
+        aCoder.encodeBool(status, forKey: "todoStatus")
+        aCoder.encodeObject(deadline, forKey: "todoDeadline")
+        aCoder.encodeInt(weight!, forKey: "todoWeight")
+        aCoder.encodeObject(createdAt, forKey: "todoCreatedAt")
+        aCoder.encodeObject(doneAt, forKey: "todoDoneAT")
     }
 }
 
